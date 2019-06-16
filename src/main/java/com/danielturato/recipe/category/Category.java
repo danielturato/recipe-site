bb429e4a5097d87c77d345b29a056b84b9901ae0
Daniel <dturato@hotmail.com>
2019-06-16 19:08:39 +0100

    Fixed bug with category enum values being displayed

--- src/main/java/com/danielturato/recipe/category/Category.java
+++ src/main/java/com/danielturato/recipe/category/Category.java
@@ -1,9 +1,16 @@
 package com.danielturato.recipe.category;
 
 public enum Category {
-    ALL_CATEGORIES,
-    BREAKFAST,
-    LUNCH,
-    DINNER,
-    DESSERT
+    ALL_CATEGORIES("All Categories"),
+    BREAKFAST("Breakfast"),
+    LUNCH("Lunch"),
+    DINNER("Dinner"),
+    DESSERT("Dessert");
+
+    public final String label;
+
+    private Category(String label) {
+        this.label = label;
+    }
+
 }
